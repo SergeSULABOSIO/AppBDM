aadb459d05eb7150f47a926f6c13a99b946eb759
Serge SULA <sulabosiog@gmail.com>
2022-12-16 03:55:11 +0100

    Creation base de donnees BDM et table Entreprise avec success

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -3,16 +3,49 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route("/entreprise")]
 class EntrepriseController extends AbstractController
 {
-    #[Route('/entreprise', name: 'entreprise')]
-    public function index(): Response
+    #[Route('/list', name: 'entreprise.list')]
+    public function list(Request $request): Response
     {
-        return $this->render('entreprise/index.html.twig', [
-            'controller_name' => 'EntrepriseController',
+        $session = $request->getSession();
+        $appTitreRubrique = "Entreprise / List";
+        $this->addFlash('success', "Bien venu sur BDM!");
+        
+        return $this->render('entreprise/entreprise.edit.html.twig', 
+        [
+            'appTitreRubrique' => $appTitreRubrique
+        ]);
+    }
+
+    #[Route('/edit', name: 'entreprise.edit')]
+    public function edit(Request $request): Response
+    {
+        $session = $request->getSession();
+        $appTitreRubrique = "Entreprise / Edit";
+        $this->addFlash('success', "Bien venu sur BDM!");
+        
+        return $this->render('entreprise/entreprise.edit.html.twig', 
+        [
+            'appTitreRubrique' => $appTitreRubrique
+        ]);
+    }
+
+    #[Route('/delete', name: 'entreprise.delete')]
+    public function delete(Request $request): Response
+    {
+        $session = $request->getSession();
+        $appTitreRubrique = "Entreprise / Delete";
+        $this->addFlash('success', "Bien venu sur BDM!");
+        
+        return $this->render('entreprise/entreprise.edit.html.twig', 
+        [
+            'appTitreRubrique' => $appTitreRubrique
         ]);
     }
 }
---
+++ src/Entity/Entreprise.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EntrepriseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EntrepriseRepository::class)]
+class Entreprise
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $telephone = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $rccm = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $idnat = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $numipot = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(?string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(?string $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getRccm(): ?string
+    {
+        return $this->rccm;
+    }
+
+    public function setRccm(?string $rccm): self
+    {
+        $this->rccm = $rccm;
+
+        return $this;
+    }
+
+    public function getIdnat(): ?string
+    {
+        return $this->idnat;
+    }
+
+    public function setIdnat(?string $idnat): self
+    {
+        $this->idnat = $idnat;
+
+        return $this;
+    }
+
+    public function getNumipot(): ?string
+    {
+        return $this->numipot;
+    }
+
+    public function setNumipot(?string $numipot): self
+    {
+        $this->numipot = $numipot;
+
+        return $this;
+    }
+}
