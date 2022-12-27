7ad57d7ba37d61bd9b4465ad9f66078ae62329e5
Serge - Bureau <sulabosiog@gmail.com>
2022-12-27 17:14:52 +0100

    Contact - validation - OK

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Client;
 use App\Entity\Contact;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
