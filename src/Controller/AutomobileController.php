2c5ed7a1c5c2a4150cfa54f039fa63358f62a967
Serge - Bureau <sulabosiog@gmail.com>
2022-12-30 16:00:53 +0100

    Assureurs et Automobile - Pagination et détails simple - OK

--- src/Controller/AutomobileController.php
+++ src/Controller/AutomobileController.php
@@ -5,32 +5,48 @@ namespace App\Controller;
 use App\Entity\Automobile;
 use App\Form\AutomobileFormType;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 #[Route("/automobile")]
 class AutomobileController extends AbstractController
 {
 
-    #[Route('/', name: 'automobile.list')]
-    public function list(Request $request): Response
+    #[Route('/list/{page?1}/{nbre?20}', name: 'automobile.list')]
+    public function list(Request $request, ManagerRegistry $doctrine, $page, $nbre, PaginatorInterface $paginatorInterface): Response
     {
         $session = $request->getSession();
         $appTitreRubrique = "Automobile";
-        //$this->addFlash('success', "Bien venu sur BDM!");
+        $repository = $doctrine->getRepository(Automobile::class);
+        $data = $repository->findAll();
+        $automobiles = $paginatorInterface->paginate($data, $page, $nbre);
+
 
         return $this->render(
             'automobile.list.html.twig',
             [
-                'appTitreRubrique' => $appTitreRubrique
+                'appTitreRubrique' => $appTitreRubrique,
+                'automobiles' => $automobiles
             ]
         );
     }
 
 
+
+    #[Route('/details/{id<\d+>}', name: 'automobile.details')]
+    public function detail(Automobile $automobile = null): Response
+    {
+        if ($automobile) {
+            return $this->render('automobile.details.html.twig', ['automobile' => $automobile]);
+        } else {
+            $this->addFlash('error', "Désolé. Cet enregistrement est introuvable.");
+            return $this->redirectToRoute('automobile.list');
+        }
+    }
 
 
 
@@ -57,8 +73,8 @@ class AutomobileController extends AbstractController
             $entityManager = $doctrine->getManager();
             $entityManager->persist($automobile);
             $entityManager->flush();
-            $this->addFlash('success', "Bravo ! " . $automobile->getMarque() . " vient d'être " . $adjectif . " avec succès.");
-            return $this->redirectToRoute('automobile.edit');
+            $this->addFlash('success', "Bravo ! " . $automobile->getMarque(). " / " . $automobile->getPlaque() . " vient d'être " . $adjectif . " avec succès.");
+            return $this->redirectToRoute('automobile.list');
         } else {
 
             return $this->render(
