59fcede14b4e65db909b7c9882378c5fbf5395b8
Serge SULA <sulabosiog@gmail.com>
2023-01-01 00:31:49 +0100

    Contact - pagination et détails - OK

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,29 +5,47 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactFormType;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 #[Route("/contact")]
 class ContactController extends AbstractController
 {
 
-    #[Route('/', name: 'contact.list')]
-    public function list(Request $request): Response
+    #[Route('/list/{page?1}/{nbre?20}', name: 'contact.list')]
+    public function list(Request $request, ManagerRegistry $doctrine, $page, $nbre, PaginatorInterface $paginatorInterface): Response
     {
         $session = $request->getSession();
         $appTitreRubrique = "Contact";
-        //$this->addFlash('success', "Bien venu sur BDM!");
+        $repository = $doctrine->getRepository(Contact::class);
+        $data = $repository->findAll();
+        $contacts = $paginatorInterface->paginate($data, $page, $nbre);
+
 
         return $this->render(
             'contact.list.html.twig',
             [
-                'appTitreRubrique' => $appTitreRubrique
+                'appTitreRubrique' => $appTitreRubrique,
+                'contacts' => $contacts
             ]
         );
+    }
+
+
+
+    #[Route('/details/{id<\d+>}', name: 'contact.details')]
+    public function detail(Contact $contact = null): Response
+    {
+        if ($contact) {
+            return $this->render('contact.details.html.twig', ['contact' => $contact]);
+        } else {
+            $this->addFlash('error', "Désolé. Cet enregistrement est introuvable.");
+            return $this->redirectToRoute('contact.list');
+        }
     }
 
 
@@ -58,7 +76,7 @@ class ContactController extends AbstractController
             $entityManager->persist($contact);
             $entityManager->flush();
             $this->addFlash('success', "Bravo ! " . $contact->getNom() . " vient d'être " . $adjectif . " avec succès.");
-            return $this->redirectToRoute('contact.edit');
+            return $this->redirectToRoute('contact.list');
         } else {
 
             return $this->render(
